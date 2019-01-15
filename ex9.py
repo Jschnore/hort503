e91599bfcca3c9b7492a2e712a081bc6fc8632b2
Jschnore <46575911+Jschnore@users.noreply.github.com>
2019-01-14 20:58:02 -0800

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,16 @@
+# Here's some strange stuff, I'm not exactly typing some of the unimportant stuff
+
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("here are the days: ", days)
+print("here are the months: ", months)
+
+print("""
+There's something going on Here
+with the three double quotes
+I kinda like it
+Its like 6 apples on top
+my kids like that book
+""")
